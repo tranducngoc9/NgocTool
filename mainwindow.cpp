67abfbaebd2186c5ef145f0d0672986339b62589
Đức Ngọc <tk632k@gmail.com>
2023-09-13 23:29:07 +0700

    ok

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QFileDialog"
-#include "QDebug"
+
+#include <QListWidget>
+#include <QListWidgetItem>
+//#include <QTextEdit>
+#include <QLineEdit>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<< "Main thread: "<<QThread::currentThread();
 }
 
 MainWindow::~MainWindow()
@@ -19,9 +24,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QStringList lsFileList = QFileDialog::getOpenFileNames(this, "Select PNG Files", "", tr("PNG ngoctest Files (*.png)"));
-
-
+    QString path = QFileDialog::getExistingDirectory(this, tr("Open Directory"));
+    qDebug() << path;
+    ui->numFile->setText(QString::number( QDir(path).count()) );
 }
 
 
@@ -30,10 +35,64 @@ void MainWindow::on_actionOpen_triggered()
 
 void MainWindow::on_mouseControl_clicked()
 {
+    int startX = 0; // Tọa độ X ban đầu
+    int startY = 0; // Tọa độ Y ban đầu
     int targetX = 500; // Tọa độ X mục tiêu
-       int targetY = 300; // Tọa độ Y mục tiêu
+    int targetY = 300; // Tọa độ Y mục tiêu
+    int numSteps = 100; // Số bước di chuyển
 
-       // Di chuyển con trỏ chuột đến tọa độ mục tiêu
-       SetCursorPos(targetX, targetY);
+    for (int i = 0; i <= numSteps; ++i) {
+        int currentX = startX + (targetX - startX) * i / numSteps;
+        int currentY = startY + (targetY - startY) * i / numSteps;
+
+        SetCursorPos(currentX, currentY);
+        Sleep(10); // Thời gian ngừng giữa các bước (milliseconds)
+    }
+}
+
+
+void MainWindow::on_getPosMouse_clicked()
+{
+    isPress = false;
+    QThread * thread1 = QThread::create([=](){
+        qDebug()<< "Runing thread 1: "<<QThread::currentThread();
+        while(!isPress){
+            int x =  QCursor::pos().x();
+            int y = QCursor::pos().y();
+            ui->labelCoordinates->setText( QString("(%1,%2)").arg(QString::number(x),QString::number(y) ));
+            Sleep(100);
+        }
+        qDebug()<< "End thread 1: "<<QThread::currentThread();
+    });
+
+    thread1->start();
+
+}
+
+
+
+void MainWindow::on_quitGetPosition_clicked()
+{
+    isPress = true;
+}
+
+
+void MainWindow::on_addItem_clicked()
+{
+    ui->listWidget->setFlow(QListView::LeftToRight);
+    ui->listWidget->setGridSize(QSize(110, 90));
+    ui->listWidget->setResizeMode(QListView::Adjust);
+    ui->listWidget->setViewMode(QListView::ListMode);
+    ui->listWidget->setWrapping(true);
+        auto    item = new QListWidgetItem("", ui->listWidget);
+        auto    text = new QLineEdit("");
+        text->setMinimumSize(100, 80);
+        ui->listWidget->setItemWidget(item, text);
+}
+
+
+void MainWindow::on_btnRun_clicked()
+{
+    qDebug()<< ui->listWidget->takeItem(0)->data(2);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <windows.h>
+#include <QDir>
+#include <QThread>
+#include "QFileDialog"
+#include "QDebug"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,7 +26,17 @@ private slots:
 
     void on_mouseControl_clicked();
 
+    void on_getPosMouse_clicked();
+
+
+    void on_quitGetPosition_clicked();
+
+    void on_addItem_clicked();
+
+    void on_btnRun_clicked();
+
 private:
+    bool isPress = false;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
